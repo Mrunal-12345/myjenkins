8aa19e3c3138cf9f27b6809a666f9f3859559fb6
Mrunal-12345 <95688935+Mrunal-12345@users.noreply.github.com>
2022-04-12 13:52:05 +0530

    Create pipeline.groovy

---
+++ pipeline.groovy
@@ -0,0 +1,20 @@
+pipeline {
+    agent any 
+    stages {
+        stage('pull') { 
+            steps {
+                 git credentialsId: '07deea9e-9d03-490b-8745-1cc1192efdc6', url: 'https://github.com/Mrunal-12345/studentapp-ui.git'
+            }
+        }
+        stage('Build') { 
+            steps {
+                 sh 'mvn clean package'
+            }
+        }
+        stage('Test') { 
+            steps {
+                echo " yes kaila dublu "
+            }
+        }
+    }
+}
